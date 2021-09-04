5598694255abc51a071c0fc6008387e009fe49b7
ilbokov <ilbokov@yandex.ru>
2021-09-04 18:55:58 +0300

    test workflow with push to server

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось, причем вместе с сообщением в телеграм!')
 
 
 def second_page(request):
